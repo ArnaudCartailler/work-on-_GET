9a0b11d61e49421ac888fdd498b27d2806408e93
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-06 09:42:36 +0200

    working on URL safety

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+			<title>Ma page web</title>
+		</head>
+		<body>
+			
+
+			
+				<a href="bonjour.php?nom=Doe&amp;prenom=John&amp;repeter=">Bonjour </a>
+			
+			
+		
+		</body>
+	</html>
+
+
